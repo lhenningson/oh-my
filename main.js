c6f5cfb6d30ac23f038c9163d429d8305d8b2353
lhenningson <l.henningson@share.epsb.ca>
2022-11-07 11:23:06 -0700

    bgheruq

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-// JavaScript Code
+document.getElementById("8-ball").addEventListener("click", Answer);
+
+let answ = document.getElementById("ans");
+let quest = document.getElementById("Qst");
+
+
+function Answer() {
+    let randnum = Math.random();
+
+    if (randnum < 0.2) {
+        answ.innerHTML = "Without a Doubt";
+    } else if (randnum < 0.4) {
+        answ.innerHTML = "As I see it, yes"
+    } else if (randnum < 0.6) {
+        answ.innerHTML = "Concentrate and ask again"
+    } else if (randnum < 0.8) {
+        answ.innerHTML = "Don't count on it"
+    } else if (randnum < 1) {
+        answ.innerHTML = "Outlook not so good"
+    } 
+}
