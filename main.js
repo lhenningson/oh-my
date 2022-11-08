9b6b699de3a1b7e0c59d14a17690fd012f586974
lhenningson <l.henningson@share.epsb.ca>
2022-11-08 11:22:20 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,27 @@
-document.getElementById("8-ball").addEventListener("click", Answer);
-
 let answ = document.getElementById("ans");
 let quest = document.getElementById("Qst");
 
 
-function Answer() {
-    let randnum = Math.random();
+document.getElementById("8-ball").addEventListener("click", Answer);
 
+
+
+
+function Answer() {
+  if (quest = "") {
+    answ.innerHTML = "Please ask a question";
+  } else {
+    let randnum = Math.random()
     if (randnum < 0.2) {
         answ.innerHTML = "Without a Doubt";
     } else if (randnum < 0.4) {
-        answ.innerHTML = "As I see it, yes"
+        answ.innerHTML = "As I see it, yes";
     } else if (randnum < 0.6) {
-        answ.innerHTML = "Concentrate and ask again"
+        answ.innerHTML = "Concentrate and ask again";
     } else if (randnum < 0.8) {
-        answ.innerHTML = "Don't count on it"
+        answ.innerHTML = "Don't count on it";
     } else if (randnum < 1) {
-        answ.innerHTML = "Outlook not so good"
+        answ.innerHTML = "Outlook not so good";
     } 
+}
 }
